c667a7ef133101d5aaf66ce7c99fb4a7eac16c54
mengguoru <2506839494@qq.com>
2019-12-29 09:53:32 +0800

    init

---
+++ src/main/java/com/meng/testspringboot2/Testspringboot2Application.java
@@ -0,0 +1,13 @@
+package com.meng.testspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testspringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testspringboot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/meng/testspringboot2/Testspringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.testspringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testspringboot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
